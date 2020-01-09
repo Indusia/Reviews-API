356a01902cda846a58c395aa71ad38065deb3606
David <david.lam296@gmail.com>
2020-01-09 12:21:07 -0500

    Create database connection; remove unnecessary files; update project

--- db/index.js
+++ db/index.js
@@ -1,10 +1,11 @@
 const pgp = require("pg-promise")({});
+
 const connection = {
-  host: "db",
+  host: process.env.HOST || "localhost",
   port: 5432,
   database: process.env.DB || "reviews",
-  user: process.env.USER || "postgres",
-  password: process.env.PASSWORD || "postgres"
+  user: process.env.USER || "uncreative",
+  password: process.env.PASSWORD || null
 };
 
 let db = pgp(connection);
